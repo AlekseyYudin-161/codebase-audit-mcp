6273ef8ec2fd5ffd8523ce1d05507423f3158eec
aleksey_y <110169963+farogu@users.noreply.github.com>
2026-03-01 17:25:05 +0300

    Unhiddencmd 'smoke'

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys, uvicorn
-# from server.smoke import run_smoke
+from server.smoke import run_smoke
 from server.logging_config import logger
 
 if __name__ == "__main__":
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     if cmd == "serve":
         logger.info("Starting server...")
         uvicorn.run("server.app:app", host="0.0.0.0", port=8000)
-    # elif cmd == "smoke":
-    #     sys.exit(run_smoke())
+    elif cmd == "smoke":
+        sys.exit(run_smoke())
     else:
         logger.error(f"Unknown command: {cmd}. Use: serve | smoke")
         print(f"Unknown command: {cmd}. Use: serve | smoke")
